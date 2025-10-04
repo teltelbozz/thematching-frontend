adce34a91a8129d3ce8b780f03bacb29270a294e
mulder <mulder@muldernoMacBook-Pro-2.local>
2025-10-04 10:08:26 +0900

    202510040949

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
-// src/main.tsx
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './styles.css'; // グローバルCSS
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
