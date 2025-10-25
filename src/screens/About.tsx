f750e937ce38cfdac9630f8f2a64cd7d028ba1c6
mulder <mulder@muldernoMacBook-Pro-2.local>
2025-10-25 18:03:37 +0900

    202510251803

--- src/screens/About.tsx
+++
@@ -1,9 +0,0 @@
-// src/screens/About.tsx
-export default function About() {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h1>About Page</h1>
-      <p>Service overview will be added here.</p>
-    </div>
-  );
-}
--- src/screens/Faq.tsx
+++
@@ -1,9 +0,0 @@
-// src/screens/Faq.tsx
-export default function Faq() {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h1>FAQ</h1>
-      <p>Frequently asked questions will be added here.</p>
-    </div>
-  );
-}
