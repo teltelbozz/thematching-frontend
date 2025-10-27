3405741a91d4ce0226636025227021bd116ee803
mulder <mulder@muldernoMacBook-Pro-2.local>
2025-10-28 00:18:05 +0900

    202510280018

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles.css';
 
 // --- 追加: どんな未捕捉エラーもログ&画面表示する ---
 function installGlobalErrorTrap() {
