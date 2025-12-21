a1c1ecd5655a5bcc2beac7a77509d66486d489a8
mulder <mulder@muldernoMacBook-Pro-2.local>
2025-12-21 11:51:45 +0900

    202512211151

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,8 +2,8 @@
 export default function Home() {
   return (
     <div style={{ padding: 16 }}>
-      <h1>ようこそ 👋</h1>
-      <p>ログイン済みです。プロフィール登録済みの場合はこちらのホームに来ます。</p>
+      <h1>ようこそ 🍷</h1>
+      <p>LINEでログインしました。</p>
     </div>
   );
 }
